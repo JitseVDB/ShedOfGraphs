7e3b2e98f6afde66ca7be9594040ce5e969dcc9d
JitseVDB <jitse.vandenberghe@outlook.be>
2025-04-30 11:10:39 +0200

    Fixed path to filter_graph.py

--- graph_processing/run_filter.sh
+++ graph_processing/run_filter.sh
@@ -15,4 +15,4 @@ echo "$FILTER_STRING"
 echo "Filtered graphs will be printed to stdout, and history will be saved to history.txt."
 
 # Run geng and pipe output into the Python filter
-geng $ORDER | python3 filter_graph.py "$FILTER_STRING"
+geng $ORDER | python3 ./graph_processing/filter_graph.py "$FILTER_STRING"
